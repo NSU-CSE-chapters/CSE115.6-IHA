ffe34e58b77ba53534d60625307ce95d95de04e3
FahimMuntashir <fahim.tech.me@gmail.com>
2020-07-14 22:04:32 +0600

    lab 02

---
+++ LAB problem solving/task1.c
@@ -0,0 +1,14 @@
+ //Name: Fahim Muntashir
+//ID: 2021183642
+//Course code: CSE115L
+//Section: 06
+
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Textline with \" in it\n");
+    printf("\tTabbed textline with \\n in it\n");
+
+}
---
+++ LAB problem solving/task2.c
@@ -0,0 +1,33 @@
+ //Name: Fahim Muntashir
+//ID: 2021183642
+//Course code: CSE115L
+//Section: 06
+
+
+#include<stdio.h>
+
+int main()
+{
+    int i=139;
+    double d=5243.873649;
+    char c= 'X';
+
+    //a
+    printf("Integer value: %d\n", i);
+    printf("Octal value: %o\n", i);
+    printf("Hexadecimal value: %X\n\n", i);
+
+    //b
+
+    printf("double value with E notation : %E\n", d);
+
+    //c
+
+    printf("double value: %011.3lf\n",d );
+
+    //d
+
+    printf("charactar value: %c\n", c);
+    printf("char value of decimal:  %d\n", c);
+
+}
