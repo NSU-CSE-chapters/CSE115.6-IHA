6fde03a2ce7e27691c7e753d971b2aee95acce03
FahimMuntashir <fahim.tech.me@gmail.com>
2020-07-15 02:38:42 +0600

    reverse number

---
+++ Programiz Problems/AtoZchar.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch;
+    int count = 26;
+
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+       
+            printf("%c\n", ch);
+    
+        
+        
+    }
+    
+}
---
+++ Programiz Problems/reverseNum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int number, reminder, reverse=0;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    while (number!=0)
+    {
+        reminder = number%10;
+        reverse = reverse * 10 +reminder;
+        number = number/10;
+
+    }
+    printf("Reverse number is: %d\n", reverse);
+}
