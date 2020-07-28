a7d19a518fa490562cff28c600a9c26cf6b9bd3c
FahimMuntashir <fahim.tech.me@gmail.com>
2020-07-29 01:21:50 +0600

    pattern and pyramids

---
+++ Week -4/pyramidWspace2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    printf("enter N: ");
+    scanf("%d",&n);
+
+    for (int i =n; i>=1; i--)
+    {
+        for (int j= 1; j<=n-i;j++)
+        {
+            printf(" ");
+        }
+
+        for (int j=1;j<=i;j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+        
+        
+    }
+    
+}
