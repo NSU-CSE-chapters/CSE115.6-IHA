a3cb2f52f0be63a4775b54fa88e2eba35fe109e2
FahimMuntashir <fahim.tech.me@gmail.com>
2020-08-21 01:59:14 +0600

    string function

---
+++ Week -8/strcat.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str1[20]= "welcome to ";
+    char str2[20] = "our academy";
+    stpcpy(str2, str1);
+    stpcpy(str1, str2);
+    strcat(str1,str2);
+
+    printf("%s", str1);
+
+}
---
+++ Week -8/strlen.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char str[]= "Hello World";
+
+    int length;
+
+    length = strlen(str);
+
+    printf("%d\n", length);
+
+    return 0;
+}
