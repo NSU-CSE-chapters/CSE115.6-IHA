b59db7aff3090da196b35690ad826ef0d7fd7bf7
FahimMuntashir <fahim.tech.me@gmail.com>
2020-09-13 02:08:20 -0700

    file user input

---
+++ Week -11/fileUserInput.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp;
+    char ch;
+    printf("data input\n");
+
+    fp = fopen("input", "w");
+
+    while ((ch=getchar())!=EOF)
+    {
+        putc(ch, fp);
+
+    }
+
+    fclose(fp);
+    printf("data output\n");
+
+    fp = fopen("input", "r");
+
+    while ((ch=getc(fp))!=EOF)
+    {
+        printf("%c", ch);
+    }
+    fclose(fp);
+    
+    
+}
